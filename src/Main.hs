be33239e6219fcae46bab3e6fdb441b416086c17
Otávio Pace <otaviopp8@gmail.com>
2019-05-05 12:16:47 -0300

    parse: add function with helpers

--- src/Main.hs
+++ src/Main.hs
@@ -2,13 +2,16 @@ import System.IO (hFlush, stdout)
 import Data.Map (Map)
 import Data.List.Utils (replace)
 import Data.List.Split (splitOn)
+import Text.Read (readMaybe)
 
 -- pipeline operator
 x |> f = f x
 
 data Expr = Symbol String | Number Float | List [Expr]
+  deriving(Show)
 
 data Err = Err { reason :: String }
+  deriving(Show)
 
 data Env = Env { data' :: (Map String Expr) }
 
@@ -19,17 +22,33 @@ tokenize expr = expr
   |> splitOn " "
   |> filter (not . null)
 
+parse :: [String] -> Either Err (Expr, [String])
+parse [] = Left Err { reason = "Could not get token, string is empty" }
+parse (token:rest)
+  | token == "(" = read_seq rest []
+  | token == ")" = Left Err { reason = "Unexpected `)` token" }
+  | otherwise = Right ((parse_atom token), rest)
+
+read_seq :: [String] -> [Expr] -> Either Err (Expr, [String])
+read_seq [] _ = Left Err { reason = "Could not find closing `)`" }
+read_seq (next_token:rest) expr_list
+  | next_token == ")" = Right (List expr_list, rest)
+  | otherwise = case (parse ([next_token] ++ rest)) of
+                  Right (expr, tokens) -> read_seq tokens (expr_list ++ [expr])
+                  Left err -> Left err
+
+parse_atom :: String -> Expr
+parse_atom atom = case (readMaybe atom) of
+                    Nothing -> Symbol atom
+                    Just v -> Number v
+
 repl = do
   putStr "hisp > "
   hFlush stdout
   input <- getLine
   if input == ".exit"
      then return ()
-     else match_input input >> repl
-
-match_input :: String -> IO ()
-match_input input
-  | otherwise = return ()
+     else print (parse (tokenize input)) >> repl
 
 main = do
   repl

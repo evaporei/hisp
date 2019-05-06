d0e1ff2ced72243df4b4750530040b46d0258f33
Otávio Pace <otaviopp8@gmail.com>
2019-05-06 00:10:31 -0300

    main: fix naming convention

--- src/Main.hs
+++ src/Main.hs
@@ -44,59 +44,59 @@ tokenize expr = expr
 parse :: [String] -> Either Err (Expr, [String])
 parse [] = Left Err { reason = "Could not get token, string is empty" }
 parse (token:rest)
-  | token == "(" = read_seq rest []
+  | token == "(" = readSeq rest []
   | token == ")" = Left Err { reason = "Unexpected `)` token" }
-  | otherwise = Right ((parse_atom token), rest)
+  | otherwise = Right ((parseAtom token), rest)
 
-read_seq :: [String] -> [Expr] -> Either Err (Expr, [String])
-read_seq [] _ = Left Err { reason = "Could not find closing `)`" }
-read_seq (next_token:rest) expr_list
-  | next_token == ")" = Right (List expr_list, rest)
-  | otherwise = case (parse ([next_token] ++ rest)) of
-                  Right (expr, tokens) -> read_seq tokens (expr_list ++ [expr])
+readSeq :: [String] -> [Expr] -> Either Err (Expr, [String])
+readSeq [] _ = Left Err { reason = "Could not find closing `)`" }
+readSeq (nextToken:rest) exprList
+  | nextToken == ")" = Right (List exprList, rest)
+  | otherwise = case (parse ([nextToken] ++ rest)) of
+                  Right (expr, tokens) -> readSeq tokens (exprList ++ [expr])
                   Left err -> Left err
 
-parse_atom :: String -> Expr
-parse_atom atom = case (readMaybe atom) of
+parseAtom :: String -> Expr
+parseAtom atom = case (readMaybe atom) of
                     Nothing -> Symbol atom
                     Just v -> Number v
 
-is_number :: Expr -> Bool
-is_number expr = case expr of
+isNumber :: Expr -> Bool
+isNumber expr = case expr of
                    Number n -> True
                    _ -> False
 
-sum'_aux :: Float -> Expr -> Float
-sum'_aux acc expr = case expr of
+sum'Aux :: Float -> Expr -> Float
+sum'Aux acc expr = case expr of
                    Number n -> acc + n
                    _ -> error "Should not sum Expressions that are not Numbers"
 
 sum' :: [Expr] -> Either Err Expr
 sum' [] = Left Err { reason = "Could not sum, list expression is empty" }
-sum' expr_list = case (any is_number expr_list) of
+sum' exprList = case (any isNumber exprList) of
                    False -> Left Err { reason = "Could not sum, not all expressions in list are Numbers" }
-                   True -> Right (Number (foldl sum'_aux 0.0 expr_list))
+                   True -> Right (Number (foldl sum'Aux 0.0 exprList))
 
-get_expr_number :: Expr -> Float
-get_expr_number expr = case expr of
+getExprNumber :: Expr -> Float
+getExprNumber expr = case expr of
                          Number n -> n
                          _ -> error "Could not get Number out of an Expression that is not a Number"
 
 subtract' :: [Expr] -> Either Err Expr
 subtract' [] = Left Err { reason = "Could not subtract, list expression is empty" }
-subtract' (x:xs) = case (any is_number ([x] ++ xs)) of
+subtract' (x:xs) = case (any isNumber ([x] ++ xs)) of
                    False -> Left Err { reason = "Could not subtract, not all expressions in list are Numbers" }
-                   True -> Right (Number ((get_expr_number x) - (foldl sum'_aux 0.0 xs)))
+                   True -> Right (Number ((getExprNumber x) - (foldl sum'Aux 0.0 xs)))
 
-default_env = Env {
+defaultEnv = Env {
   data' = Data.Map.fromList [
     ("+", Func sum'),
     ("-", Func subtract')
                             ]
                   }
 
-call_eval_on_arg :: Env -> Expr -> Either Err Expr
-call_eval_on_arg env expr = eval env expr
+callEvalOnArg :: Env -> Expr -> Either Err Expr
+callEvalOnArg env expr = eval env expr
 
 eval :: Env -> Expr -> Either Err Expr
 eval env expr = case expr of
@@ -109,10 +109,10 @@ eval env expr = case expr of
                                  False -> case (eval env (head list)) of
                                             Left err -> Left err
                                             Right expr -> case expr of
-                                                            Func func -> let eval_args = (map (call_eval_on_arg env) (tail list))
-                                                                          in case (any isLeft eval_args) of
-                                                                               True -> Left (head (lefts eval_args))
-                                                                               False -> func (rights eval_args)
+                                                            Func func -> let evalArgs = (map (callEvalOnArg env) (tail list))
+                                                                          in case (any isLeft evalArgs) of
+                                                                               True -> Left (head (lefts evalArgs))
+                                                                               False -> func (rights evalArgs)
                                                             _ -> Left Err { reason = "First form must be a function" }
                   Func _ -> Left Err { reason = "Unexpected form" }
 
@@ -125,7 +125,7 @@ repl = do
      then return ()
      else case (parse (tokenize input)) of
             Left err -> print (reason err) >> repl
-            Right (expr, _) -> case (eval default_env expr) of
+            Right (expr, _) -> case (eval defaultEnv expr) of
                                  Left l -> print l >> repl
                                  Right r -> print r >> repl
 
